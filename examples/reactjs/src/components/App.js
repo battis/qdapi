4f18f5a9c7e6fb785aaf54a602c005ba3f46a812
Seth Battis <seth@battis.net>
2017-04-12 10:32:33 -0400

    Simplify React Example

--- examples/reactjs/src/components/App.js
+++ examples/reactjs/src/components/App.js
@@ -18,6 +18,12 @@ export default class App extends Component {
     );
   }
 
+  fetchJSON(url, params, callback) {
+      fetch(url, params).then(function(response) {
+          return response.json()
+      }).then(callback)
+  }
+
   replace(id, text) {
       document.getElementById(id).innerHTML = text;
   }
@@ -27,13 +33,11 @@ export default class App extends Component {
   }
 
   componentDidMount() {
-      var url = 'https://roswell.stmarksschool.org/~online-inventory/api/v1/foo'
+      var url = 'https://example.com/api/v1/foo'
       var self = this
 
       /* GET all of the data in the foo table */
-      fetch(url).then(function(response) {
-          return response.json()
-      }).then(function(foos) {
+      this.fetchJSON(url,{method: 'get'}, function(foos) {
           self.replace('get-all', foos.map(self.format).join('<br/>'))
           /*
            * output:
@@ -42,30 +46,26 @@ export default class App extends Component {
            * id: 2; bar: the quick brown fox; baz: asparagus
            * id: 3; bar: red; baz: raspberry
            */
-      }).catch(error => self.replace('get-all', error))
+      })
 
       /* GET a single record */
-      fetch(url + '/2').then(function(response) {
-          return response.json()
-      }).then(function(foo) {
+      this.fetchJSON(url + '/2', {method: 'get'}, function(foo) {
           self.replace('get-one', self.format(foo))
           /*
            * output
            *
            * id: 2; bar: the quick brown fox; baz: asparagus
            */
-      }).catch(error => self.replace('get-one', error))
+      })
 
       /* POST a new record */
-      fetch(url, {
+      this.fetchJSON(url, {
           method: 'post',
           body: JSON.stringify({
               bar: 'dummy info',
               baz: 'more dummy info'
           })
-      }).then(function(response) {
-          return response.json()
-      }).then(function(foo) {
+      }, function(foo) {
           self.replace('post', self.format(foo))
           /*
            * output:
@@ -74,14 +74,12 @@ export default class App extends Component {
            */
 
           /* PUT updates to an existing record (the one we just inserted!) */
-          fetch(url + '/' + foo.id, {
+          self.fetchJSON(url + '/' + foo.id, {
               method: 'put',
               body: JSON.stringify({
                   baz: 'smart info!'
               })
-          }).then(function (response) {
-              return response.json()
-          }).then(function(foo) {
+          }, function(foo) {
               self.replace('put', self.format(foo))
               /*
                * output:
@@ -90,21 +88,19 @@ export default class App extends Component {
                */
 
               /* DELETE deletes an existing record */
-              fetch(url + '/' + foo.id, {
+              self.fetchJSON(url + '/' + foo.id, {
                   method: 'DELETE',
                   body: JSON.stringify({})
-              }).then(function(response) {
-                  return response.json()
-              }).then(function(foo) {
-                  self.replace('delete', 'deleted ' + foo + ' records')
+              }, function(count) {
+                  self.replace('delete', 'deleted ' + count + ' records')
                   /*
                    * output:
                    *
                    * deleted 1 records
                    */
-              }).catch(error => self.replace('delete', error))
-          }).catch(error => self.replace('put', error))
-      }).catch(error => self.replace('post', error))
+              })
+          })
+      })
 
   }
 }
